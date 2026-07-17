4342ae512f93834c1d35adbe838f0e52c2fe6ff5
santoshi1105 <p.santoshirani@gmail.com>
2026-07-17 18:22:07 +0530

    Add files via upload

---
+++ 6.py
@@ -0,0 +1,8 @@
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+c=a+b
+d=a-b
+e=a*b
+print("sum of the given two numbers is ",c)
+print("difference of given two numbers is ",d)
+print("product of two given numbers is ",e)
