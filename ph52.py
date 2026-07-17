a742a94248dd6ea7964b7fef8db4cdca371b5dff
santoshi1105 <p.santoshirani@gmail.com>
2026-07-17 18:17:29 +0530

    Add files via upload

---
+++ ph52.py
@@ -0,0 +1,11 @@
+a=float(input('Enter a number'))
+b=float(input('Enter another number'))
+print("the sum of two numbers",a+b)
+print("the difference of two numbers",a-b)
+print("the product of the two numbers",a*b)
+print("The quotient of the given two numbers is",a/b)
+print("The remainder of the two numbers",a%b)
+print("The whole part of given two number",a//b)
+print("exponential form",a**b)
+
+      
